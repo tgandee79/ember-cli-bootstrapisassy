eb4365b6c6c29e581bddd1f41495ec53245a7e70
Todd Gandee <tgandee@bignerdranch.com>
2015-03-24 16:09:06 -0400

    nailed it

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-bootstrapisassy'
+  name: 'ember-cli-bootstrapisassy',
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+    var bootstrapPath   = 'bower_components/bootstrap-sass/assets/';
+    target.import(bootstrapPath + 'javascripts/bootstrap.js');
+    // // Import glyphicons from bootstrap
+    target.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.eot', { destDir: '/fonts/bootstrap' });
+    target.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.svg', { destDir: '/fonts/bootstrap' });
+    target.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.ttf', { destDir: '/fonts/bootstrap' });
+    target.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.woff', { destDir: '/fonts/bootstrap' });
+    target.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.woff2', { destDir: '/fonts/bootstrap' });
+  },
+  treeForStyles: function() {
+    var bootstrapPath = path.join(this.app.bowerDirectory, 'bootstrap-sass', 'assets/stylesheets');
+    var bootstrapTree = this.pickFiles(this.treeGenerator(bootstrapPath), {
+      srcDir: '/',
+      destDir: '/app/styles'
+    });
+    return bootstrapTree;
+  }
 };
